5de15cd6a02819b88bd0d71ec93a2a73741caf58
sbgrb <974487599@qq.com>
2020-09-27 00:14:32 +0800

    init

---
+++ precache-manifest.e10423b2fb92b95f71dc4b3df23bf3c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec650d4abc77c11efacd0a6e8e5d9cb7",
+    "url": "/mycost-React/index.html"
+  },
+  {
+    "revision": "2ddd25c823297da66c61",
+    "url": "/mycost-React/static/css/main.77a640d3.chunk.css"
+  },
+  {
+    "revision": "165bdb1f546056b435c2",
+    "url": "/mycost-React/static/js/2.d4589ccc.chunk.js"
+  },
+  {
+    "revision": "768165ae6e4455b0f754a8444c7b06fa",
+    "url": "/mycost-React/static/js/2.d4589ccc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ddd25c823297da66c61",
+    "url": "/mycost-React/static/js/main.213771d7.chunk.js"
+  },
+  {
+    "revision": "f59fff04fe33152aa694",
+    "url": "/mycost-React/static/js/runtime-main.f816b477.js"
+  }
+]);
